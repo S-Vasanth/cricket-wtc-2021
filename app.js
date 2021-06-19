341acd8305cc0cf49d516a278bf7838e75b3d718
unknown <vasanth7085@gmail.com>
2021-06-19 15:49:25 +0530

    init

---
+++ app.js
@@ -0,0 +1,47 @@
+
+const path = require('path')
+const express = require('express')
+const hbs=require('hbs')
+//const geocode = require('./utils/geocode')
+const boardcast = require('./src/utils/boardcast')
+
+const app = express()
+const port=process.env.PORT||5000
+
+//console.log(path.join(__dirname))
+//console.log(path.join(__dirname,'..'))
+//console.log(path.join(__dirname,'../public'))
+//path for con fig
+const publicDirectryPath=path.join(__dirname,'/public')
+const viewsPath=path.join(__dirname,'/templates/views')
+const partialsPath=path.join(__dirname,'/templates/partials')
+//setup handlebar engine and view location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+//setup static directory to serve
+app.use(express.static(publicDirectryPath))
+app.get('',(req,res)=>{
+  res.render('index',{
+    title:'WTC-2021',
+    name:'Vasanth'
+
+  })
+})
+ app.get('/score',(req,res)=>{
+    boardcast((error,{stat,score}={})=>{
+        if(error){
+            return res.send({error})
+        }    
+        
+        res.send({
+          score,
+          stat
+
+        })
+  
+  })
+})
+app.listen(port,()=>{
+    console.log("server is port"+port)
+})
---
+++ src/utils/boardcast.js
@@ -0,0 +1,21 @@
+const request = require("request");
+
+const boardcast = (callback) => {
+  const url ='https://cricapi.com/api/cricketScore?apikey=g0mfjr8nUPTeXnAJzxIpouLvLq93&unique_id=1249875'
+  request({ url, json: true }, (error, response ) => {
+    if (error) {
+      callback("Unable to connect server", undefined);
+    }else if (response.body.error) {
+      callback("unable to get score.", undefined);
+    }else {
+      callback(
+        (undefined),{
+          score:response.body.score,
+          stat:response.body.stat,
+        });
+    }
+    console.log(response.body)
+  });
+  
+ };
+module.exports = boardcast;
